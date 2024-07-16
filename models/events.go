1437e4fd9c8096fa6e01a45731cc20f996bb0617
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-07-16 18:40:11 -0400

    insert events into database

--- models/events.go
+++ models/events.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"example.com/events-go/db"
+	"time"
+)
 
 type Event struct {
-	Id          int
+	Id          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -14,10 +17,27 @@ type Event struct {
 var events = []Event{}
 
 func (e *Event) Save() error {
-	// later: add it to a database
-	events = append(events, *e)
+	query := `
+  INSERT INTO events (name, description, location, date_time, user_id)
+  VALUES (?, ?, ?, ?, ?)
+  `
+	stmt, err := db.DB.Prepare(query)
 
-	return nil
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.Id = id
+	return err
 }
 
 func GetAllEvents() []Event {
